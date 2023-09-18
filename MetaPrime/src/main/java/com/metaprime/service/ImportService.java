a916f8268a2cbbecb458e17f3ae71e96fafbd9f6
Dyunin Ivan <56828894+SyLeyaN@users.noreply.github.com>
2023-09-18 08:46:49 +0400

    Использование StAX вместо DOM Parser

--- MetaPrime/src/main/java/com/metaprime/service/ImportService.java
+++ MetaPrime/src/main/java/com/metaprime/service/ImportService.java
@@ -1,17 +1,12 @@
 package com.metaprime.service;
 
 
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.NodeList;
-import org.xml.sax.SAXException;
-
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-import java.io.File;
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLStreamConstants;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamReader;
+import java.io.FileInputStream;
 import java.io.IOException;
-import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
@@ -23,47 +18,56 @@ public class ImportService {
     private static String PASSWORD = "Sova1029";
     /**
      * Основной метод сервиса, который парсит XML файл,
-     * используя библиотеку DOM Parser и с помощью PreparedStatement
+     * используя библиотеку StAX и с помощью PreparedStatement
      * добавляются уникальные данные в БД
      */
     public static void main(String[] args){
-        try{
-            //Парсинг XML файла
+        //Основной запрос
+        String query = "INSERT INTO Orders (article, name, price)" +
+                "VALUES (?,?,?) ON CONFLICT ON CONSTRAINT unique_combination DO NOTHING;";
+        //Подключение в try with resources
+        try (PreparedStatement preparedStatement = DriverManager.
+                getConnection(URL,USERNAME,PASSWORD).prepareStatement(query)){
+            //StAX парсер
             String path =  "src/main/resources/Orders.xml";
-            DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
-            DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
-            Document document = documentBuilder.parse(new File(path));
-            //Создание подключения
-            Connection connection = DriverManager.getConnection(URL,USERNAME,PASSWORD);
-            //Создание запроса
-            NodeList orderList = document.getElementsByTagName("Order");
-            String query = "INSERT INTO Orders (article, name, price)" +
-                    "VALUES (?,?,?) ON CONFLICT ON CONSTRAINT unique_combination DO NOTHING;";
-            PreparedStatement preparedStatement = connection.prepareStatement(query);
-            for (int i = 0; i<orderList.getLength();i++){
-                Element el = (Element) orderList.item(i);
-                int article = Integer.parseInt(el.getElementsByTagName("article").item(0).getTextContent());
-                String name = el.getElementsByTagName("name").item(0).getTextContent();
-                int price = Integer.parseInt(el.getElementsByTagName("price").item(0).getTextContent());
-
-                preparedStatement.setInt(1,article);
-                preparedStatement.setString(2,name);
-                preparedStatement.setInt(3,price);
-                preparedStatement.addBatch();
+            XMLInputFactory xmlInputFactory = XMLInputFactory.newInstance();
+            XMLStreamReader xmlStreamReader = xmlInputFactory.createXMLStreamReader(new FileInputStream(path));
+            // Переменные для хранения данных из XML
+            int article = 0;
+            String name = null;
+            int price = 0;
+            //Парсинг XML файла
+            while(xmlStreamReader.hasNext()){
+                int event = xmlStreamReader.next();
+                //Если начинается событие, то происходит получение данных из файла
+                if (event == XMLStreamConstants.START_ELEMENT){
+                    switch (xmlStreamReader.getLocalName()){
+                        case "article"      -> article = Integer.parseInt(xmlStreamReader.getElementText());
+                        case "name"         -> name = xmlStreamReader.getElementText();
+                        case "price"        -> price = Integer.parseInt(xmlStreamReader.getElementText());
+                    }
+                    //Если заканчивается событие, то происходит добавление данных в запрос
+                } else if (event == XMLStreamConstants.END_ELEMENT) {
+                    if (xmlStreamReader.getLocalName().equals("Order")) {
+                        preparedStatement.setInt(1, article);
+                        preparedStatement.setString(2, name);
+                        preparedStatement.setInt(3, price);
+                        preparedStatement.addBatch();
+                        article = 0;
+                        name = null;
+                        price = 0;
+                    }
+                }
             }
             //Выполнение вставки
             int[] batchResult = preparedStatement.executeBatch();
-            //Закрытие ресурсов
-            preparedStatement.close();
-            connection.close();
+            System.out.println("Successful addition of data");
             //Обработка исключений, выводя информацию в консоль
-        } catch(ParserConfigurationException e) {
-            System.out.println("Exception: "+ e.getMessage());
         } catch(IOException e){
             System.out.println("Exception: "+ e.getMessage());
-        } catch(SAXException e){
-            System.out.println("Exception: "+ e.getMessage());
         } catch(SQLException e){
+            System.out.println("Exception: "+ e.getMessage());
+        } catch (XMLStreamException e) {
             System.out.println("Exception: "+ e.getMessage());
         }
     }
